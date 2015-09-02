9c80df065fc19be4ddb5de4d5fe5768b661cb082
jacobstanley <jacob.stanley@purenet.co.uk>
2015-09-02 22:26:04 +0100

    removed the crap structure

--- gui/install-terminal.sh
+++
@@ -1,9 +0,0 @@
-# check if update should be skipped
-if [ "$1" != "-no-update" ]; then
-        sudo apt-get update
-else
-        echo "-no-update flag passed, skipping apt-get update"
-fi
-
-# Install rxvt-unicode
-sudo apt-get install rxvt-unicode 
