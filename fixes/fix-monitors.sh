2c11a4e78162101d14d8c39b215ab0a49c8b1020
jacobstanley <jacob.stanley@purenet.co.uk>
2015-09-08 11:23:17 +0100

    salvagted some fix scripts from ubuntu scripts

---
+++ fixes/fix-monitors.sh
@@ -0,0 +1,8 @@
+# Set monitor positions correctly
+xrandr --output VGA-0 --auto --left-of VGA-1
+
+# Set monitor DPI correctly
+xrandr --dpi 96 
+
+# Set wallpaper correctly
+# feh --bg-center ~/.wallpaper.* --bg-center ~/.wallpaper.*
