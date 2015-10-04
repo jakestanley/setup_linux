6b35a6b7fc30662ca1f72a7bed04bdb1b33e4fbe
jacobstanley <jacob.stanley@purenet.co.uk>
2015-10-04 22:17:32 +0100

    added spotify repositories

--- add-repositories.sh
+++ add-repositories.sh
@@ -1,4 +1,6 @@
 # TODO this
 sudo add-apt-repository ppa:linrunner/tlp # power management stuff repository
 sudo add-apt-repository ppa:noobslab/icons # noobslab gorgeous flat icons repository (Ubuntu only)
+sudo apt-add-repository -y "deb http://repository.spotify.com stable non-free" # spotify
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D2C19886 && # spotify key
 sudo apt-get update 
