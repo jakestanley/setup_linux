a4260456cc9b72822f9b6f42002c81c05359c4a7
jacobstanley <jacob.stanley@purenet.co.uk>
2015-10-06 15:47:22 +0100

    completed the radio script

--- misc/radiox.sh
+++ misc/radiox.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-DIRECTORY="~/radio/"
+DIRECTORY=~/radio/
 STATION_NAME="RadioX"
 STATION_URL="http://streams2.as39202.net/RadioXUK"
 DATETAG=`date +"%d%m%Y-%H%M"`
-FILEFULLPATH="${DIRECTORY}${STATION_NAME}_${DATETAG}.mp3"
 
 echo ${DIRECTORY} | xargs mkdir
 echo "${STATION_NAME}Stream quit" | xargs screen -XS 
-echo ${FILEFULLPATH} | xargs -t screen -dmS ${STATION_NAME}Stream mplayer ${STATION_URL} -dumpstream -dumpfile
+screen -dmS ${STATION_NAME}Stream mplayer ${STATION_URL} -dumpstream -dumpfile ${DIRECTORY}${STATION_NAME}_${DATETAG}.mp3
 echo done
