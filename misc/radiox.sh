a30780207258be7c86f257b1256abbc5aadca06d
jacobstanley <jacob.stanley@purenet.co.uk>
2015-10-07 10:20:32 +0100

    fixed typo and changed to delete old episodes only

--- misc/radiox.sh
+++ misc/radiox.sh
@@ -4,10 +4,11 @@ DIRECTORY=~/radio/
 STATION_NAME="RadioX"
 STATION_URL="http://streams2.as39202.net/RadioXUK"
 DATETAG=`date +"%d%m%Y-%H%M"`
+DATETAG_OLD=`date -d "-10 days" +"%d%m%Y"`
 
 echo ${DIRECTORY} | xargs mkdir
 echo "${STATION_NAME}Stream quit" | xargs screen -XS 
 screen -dmS ${STATION_NAME}Stream mplayer ${STATION_URL} -dumpstream -dumpfile ${DIRECTORY}${STATION_NAME}_${DATETAG}.mp3
 echo "echo \"${STATION_NAME}Stream quit\" | xargs screen -XS" | at 22:00
-echo "rm -r ${DIRECTORY}${STATION_NAME}_${DATETAG}.mp3" | 23:00
+echo "rm -r ${DIRECTORY}${STATION_NAME}_${DATETAG_OLD}*" | at 23:00
 echo done
