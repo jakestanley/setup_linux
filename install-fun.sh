52ffe021e178221fbe8de050d443a2bd0c5527d7
jacobstanley <jacob.stanley@purenet.co.uk>
2015-10-08 15:32:42 +0100

    corrected no update flag on fun script

--- install-fun.sh
+++ install-fun.sh
@@ -1,4 +1,9 @@
-source ~/conf/flag-update.sh
+# check if update should be skipped
+if [ "$1" != "-no-update" ]; then
+    sudo apt-get update
+else
+    echo "-no-update flag passed, skipping apt-get update"
+fi
 
 sudo apt-get install -y cowsay
 sudo apt-get install -y fortune-mod
