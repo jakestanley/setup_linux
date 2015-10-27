1584330fe0f485f8c4e43a5d9261b00d0e572821
jacobstanley <jacob.stanley@purenet.co.uk>
2015-10-27 09:41:18 +0000

    added to install-work

--- install-work.sh
+++ install-work.sh
@@ -5,7 +5,20 @@ else
         echo "-no-update flag passed, skipping apt-get update"
 fi
 
-sudo apt-get install openjdk-8-jdk
+# install apache2
+sudo apt-get install apache2
+
+# install jdk (won't work on trusty)
+sudo apt-get install openjdk-8-jdk 
+
+# install mysql stuff
+sudo apt-get install mysql-server mysql-workbench mytop
+
+# install yum
+sudo apt-get install yum
+
+# install unixodbc and some dependencies
+sudo apt-get install unixodbc libc6 libkeyutils1 krb5-libs libcom_err libgcc1 libselinux1 libstdc++ libtool libuuidi1 openssl unixodbc zlib1g libaio1 comerr-dev krb5-*
 
 # TODO manually
 # install spring tool suite
