8a3dfd41cb86ed1deac523e5dd66111cc2da37af
Jake Stanley <prudentia713@gmail.com>
2015-12-09 16:00:28 +0000

    added kdesvn to gui common

--- install-gui-common.sh
+++ install-gui-common.sh
@@ -10,12 +10,13 @@ fi
 ./gui/install-i3.sh -no-update
 ./gui/install-rofi.sh -no-update
 ./gui/install-dunst.sh -no-update
+./gui/install-atom.sh
 # ./gui/install-lemonbar.sh -no-update
 # ./gui/install-chrome.sh
 sudo apt-get install chromium-browser
 
 # Install common GUI stuff
-sudo apt-get install rxvt-unicode git-cola gitk kdiff3 shutter lxappearance redshift pcmanfm x11-xserver-utils feh nitrogen filezilla gparted
+sudo apt-get install rxvt-unicode git-cola gitk kdiff3 kdesvn shutter lxappearance redshift pcmanfm x11-xserver-utils feh nitrogen filezilla gparted
 
 # Install common GUI packages available on Ubuntu and NOT Debian
 sudo apt-get install nvpy ultra-flat-icons-green redshift-gtk
