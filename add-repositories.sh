7ce6a9f764ed4b32ad87ecc2fe2e1514b5cf64ec
Jake Stanley <prudentia713@gmail.com>
2015-12-21 09:44:07 +0000

    added mopidy repo for spotify to add repos script

--- add-repositories.sh
+++ add-repositories.sh
@@ -3,4 +3,5 @@ sudo add-apt-repository ppa:linrunner/tlp # power management stuff repository
 sudo add-apt-repository ppa:noobslab/icons # noobslab gorgeous flat icons repository (Ubuntu only)
 sudo apt-add-repository -y "deb http://repository.spotify.com stable non-free" # spotify
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D2C19886 && # spotify key
+sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/jessie.list
 sudo apt-get update 
