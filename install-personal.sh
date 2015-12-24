5459be706adf6bdc9bf3c3f05c29ec24182a848d
Jake Stanley <prudentia713@gmail.com>
2015-12-24 12:42:20 +0000

    separated java 8 install and added deluge-gtk to personal
    openjdk 8 isn't available on 14.04, so the other packages weren't installing. i also added deluge-gtk and some of its dependencies, but it doesn't quite work for some reason...

--- install-personal.sh
+++ install-personal.sh
@@ -6,4 +6,7 @@ else
 fi
 
 # Install...
-sudo apt-get install grafx2 transmission-gtk openjdk-8-jdk steam
+sudo apt-get install grafx2 deluge-gtk libtorrent14 libtorrent-dev steam
+
+# Installing openjdk-8-jdk separately as it doesn't exist in the 14.04 repos
+sudo apt-get install openjdk-8-jdk
