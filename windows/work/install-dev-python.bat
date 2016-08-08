ba25d93c8e663fa34f9935a7e9146f79c2a4757a
Jake Stanley <prudentia713@gmail.com>
2016-08-08 10:02:37 +0100

    somewhat fixed the python install scripts

--- windows/work/install-dev-python.bat
+++ windows/work/install-dev-python.bat
@@ -1,5 +1,9 @@
-choco install python2 --x86 -y -r 
-choco install pip --x86 -y -r
+:: install python 2.7 from the website
+python -m pip install -U pip setuptools
+python -m pip install --upgrade pip --quiet
+choco install pywin32 --x86 -y -r
+python -m pip install lxml Pillow psutil pyodbc reportlab wheel xlrd pyserial python-ldap M2CryptoWin32 pycrypto --quiet
+:: jsonpath and py2exe packages don't work. they require manual install
 choco install python-wxwidgets --x86 -y -r
-choco install pycharm-community --x86 -y -r 
+choco install pycharm-community --x86 -y -r
 refreshenv
--- windows/work/install-pip.bat
+++
@@ -1,2 +0,0 @@
-python -m pip install --upgrade pip --quiet
-python -m pip install lxml Pillow psutil pyodbc reportlab wheel xlrd pyserial jsonpath pywin32 py2exe python-ldap M2CryptoWin32 pycrypto --quiet
