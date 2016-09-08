2f2ea1d8a33a1f221c5c545215854c04e974c5e6
Jake Stanley <prudentia713@gmail.com>
2016-09-08 18:50:45 +0100

    cleaned up

--- install-core.sh.LOCAL.24495.sh
+++
@@ -1,9 +0,0 @@
-# check if update should be skipped
-if [ "$1" != "-no-update" ]; then
-        sudo apt-get update
-else
-        echo "-no-update flag passed, skipping apt-get update"
-fi
-
-# Installs git, vim, screen
-sudo apt-get install git vim screen software-properties-common
